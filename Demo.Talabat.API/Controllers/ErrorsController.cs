c0c13e6aa6643cda16aadc53c22e8414c5541bf4
rndody <rndody@gmail.com>
2024-04-22 23:26:22 +0200

    addding deleted files

---
+++ Demo.Talabat.API/Controllers/ErrorsController.cs
@@ -0,0 +1,22 @@
+﻿using Demo.Talabat.API.Errors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Talabat.API.Controllers
+{
+	[Route("errors/{code}")] //the code is the status code
+	[ApiController]
+	[ApiExplorerSettings(IgnoreApi =true)]//telling swagger to ignore the documentation of this controller
+	//this controller is related to development no need to document it
+	public class ErrorsController : ControllerBase
+	{
+		public ActionResult Error(int code){
+		
+		if(code == 400) return BadRequest(new ApiResponse(400));
+		else if (code==401) return Unauthorized(new ApiResponse(401));
+		else if (code==404) return NotFound(new ApiResponse(code));
+		else return StatusCode(code);
+
+		}
+	}
+}
---
+++ Demo.Talabat.API/Helpers/Pagination.cs
@@ -0,0 +1,22 @@
+﻿using Demo.Talabat.API.DTOs;
+
+namespace Demo.Talabat.API.Helpers
+{
+	public class Pagination<T>
+	{
+
+		public int PageIndex { get; set; }
+		public int Pagesize { get; set; }
+		public int Count { get; set; }
+		public IReadOnlyList<T> Data { get; set; }
+
+		public Pagination(int pageIndex, int pageSize,int count, IReadOnlyList<T> data)
+		{
+			PageIndex = pageIndex;
+			Pagesize = pageSize;
+			Count = count;
+			Data = data;
+		}
+
+	}
+}
