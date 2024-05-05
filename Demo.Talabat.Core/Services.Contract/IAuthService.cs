831dfb044f78c2c664bc6568c11fe1538bd5851b
rndody <rndody@gmail.com>
2024-05-06 02:15:45 +0300

    Auth Service and JWT token

---
+++ Demo.Talabat.Core/Services.Contract/IAuthService.cs
@@ -0,0 +1,16 @@
+﻿using Demo.Talabat.Core.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Talabat.Core.Services.Contract
+{
+    public interface IAuthService
+    {
+        Task<string> CreateTokenAsync(ApplicationUser user, UserManager<ApplicationUser> userManager);
+                                                                                      //take object from the service UserManager to get the user roles
+    }
+}
