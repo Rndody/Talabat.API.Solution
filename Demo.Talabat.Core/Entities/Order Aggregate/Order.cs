9f5785e1b6017e56366d2a56d274aa4cf5ca8082
rndody <rndody@gmail.com>
2024-05-07 00:59:08 +0300

    Order Module -Entities

---
+++ Demo.Talabat.Core/Entities/Order Aggregate/Order.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Talabat.Core.Entities.Order_Aggregate
+{
+    public class Order : BaseEntity
+    { //will be mapped as table in database
+
+        public string BuyerEmail { get; set; } = null!;
+        public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.UtcNow;
+        public OrderStatus Status { get; set; } = OrderStatus.Pending;
+        public Address  ShippingAddress { get; set; } = null!;
+
+        // public int DeliveryMethodId { get; set; }//FK
+        #region //Navigational Properties
+        public DeliveryMethod DeliveryMethod { get; set; }//Navigational Property [ONE]
+        public ICollection<OrderItem> Items { get; set; } = new HashSet<OrderItem>();//Navigational Property [Many] 
+        #endregion
+        public decimal Subtotal { get; set; } //the price of each item * the quantity ---> the total price of the items without delivery costs
+        //[NotMapped]
+        //public decimal Total  => Subtotal + DeliveryMethod.Cost; //readonly property  {get}
+
+        public decimal GetTotal() => Subtotal + DeliveryMethod.Cost; //getter method
+        public  string PaymentIntentId { get; set; }=string.Empty;
+    }
+}
---
+++ Demo.Talabat.Core/Entities/Order Aggregate/OrderStatus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Talabat.Core.Entities.Order_Aggregate
+{
+    public enum OrderStatus
+    {
+        //the payment status [paid / pending ...]
+
+       // [EnumMember(Value ="Pending")] discuss this data annotation later 
+        Pending,
+        PaymentReceived, 
+        PaymentFailed
+
+    }
+}
